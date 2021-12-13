6a6f733227cc1c9070dbab59f372925bc012483c
ariscybertech <ariscybertech@gmail.com>
2021-12-13 03:32:53 -0500

    bloc_update

--- lib/aris/blocs/newsbloc/news_bloc.dart
+++ lib/aris/blocs/newsbloc/news_bloc.dart
@@ -1,7 +1,7 @@
-import 'package:aris_news/aris/repositories/news_repository.dart';
 import 'package:aris_news/aris/blocs/newsbloc/news_events.dart';
 import 'package:aris_news/aris/blocs/newsbloc/news_states.dart';
 import 'package:aris_news/aris/models/article_model.dart';
+import 'package:aris_news/aris/repositories/news_repository.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -9,7 +9,9 @@ class NewsBloc extends Bloc<NewsEvents, NewsStates> {
   NewsRepository newsRepositoty;
   NewsBloc({@required NewsStates initialState, @required this.newsRepositoty})
       : super(initialState) {
-    add(StartEvent());
+    on<StartEvent>((event, emit) {
+      add(StartEvent());
+    });
   }
 
   @override
@@ -26,7 +28,3 @@ class NewsBloc extends Bloc<NewsEvents, NewsStates> {
     }
   }
 }
-
-//bloc completed
-
-// let's deploy this bloc in ui
